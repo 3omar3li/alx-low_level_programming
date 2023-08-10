00613c796ff925ffbde83087e27182389e64f7b0
3omar3li <omar.ali2332@gmail.com>
2023-08-10 08:08:13 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main - main function
+ * return: the program ends with the value 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
