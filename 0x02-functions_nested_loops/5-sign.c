332963aa936eec6e9a723696d8553e89c7fafffd
3omar3li <omar.ali2332@gmail.com>
2023-08-15 05:58:41 -0700

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_sign - function to chech if n greater or equal or less than ZERO
+ * @n: parameter to be checked
+ * Return: 0 (Success)
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else
+		return (0);
+}
