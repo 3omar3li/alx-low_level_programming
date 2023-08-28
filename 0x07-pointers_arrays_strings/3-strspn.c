fb9138790339aff8dbe21b9809730538a51aaff4
3omar3li <omar.ali2332@gmail.com>
2023-08-28 10:24:09 -0700

    3. strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,6 +8,17 @@
  * Return: the number of bytes
  */
 
-int _strspn(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; s[i] != accept[j]; j++)
+		{
+			if (accept[j] == '\0')
+				return (i);
+		}
+	}
+	return (0);
 }
