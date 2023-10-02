3385de35e860d85ba32b5f5766ec7a1c0c055c49
3omar3li <omar.ali2332@gmail.com>
2023-10-02 07:10:36 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *  - function that reads a text file and prints it to
+ * read_textfile - function that reads a text file and prints it to
  *	the POSIX standard output
  * @filename: function parameter
  * @letters: function parameter
