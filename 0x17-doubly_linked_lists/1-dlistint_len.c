935e9888fd76e6d027e2134f69792fec1c6f5ead
3omar3li <omar.ali2332@gmail.com>
2023-11-10 04:31:11 -0800

    1. List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,20 +1,20 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
  * dlistint_len - Counts the number of elements in a doubly linked list
  * @h: The double linked list to count
  * Return: Number of elements in the doubly linked list
- */
+*/
+
 size_t dlistint_len(const dlistint_t *h)
 {
-	int lenght = 0;
+	int i = 0;
 
 	while (h != NULL)
 	{
-		++lenght;
+		++i;
 		h = h->next;
 	}
 
-	return (lenght);
+	return (i);
 }
