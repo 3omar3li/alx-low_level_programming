f49e9eb6ee3c273ce4517cf99765049c4a667d30
3omar3li <omar.ali2332@gmail.com>
2023-12-08 06:42:06 -0800

    6. >>> del ht

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - function that deletes a hash table
+ * @ht: the hash table
+ * Return: void
+*/
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *node = NULL;
+	hash_node_t *temp = NULL;
+
+	if (ht && ht->size && ht->array)
+	{
+		for (; i < ht->size; ++i)
+		{
+			node = ht->array[i];
+
+			if (node)
+			{
+				if (node->next)
+				{
+					node = node->next;
+					while (node)
+					{
+						temp = node;
+						node = node->next;
+						free(temp->key);
+						free(temp->value);
+						free(temp);
+					}
+				}
+
+				node = ht->array[i];
+				if (node->key && node->value)
+				{
+					free(node->key);
+					free(node->value);
+				}
+			}
+
+			free(node);
+		}
+
+		free(ht->array);
+		free(ht);
+	}
+}
