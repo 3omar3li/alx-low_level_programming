9b0b10434b76c36bd5c85595fd4b5d4602bf03c5
3omar3li <omar.ali2332@gmail.com>
2023-12-08 06:17:21 -0800

    3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,8 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int idx = 0;
-	hash_node_t *elem = NULL, *new_node = NULL;
+	hash_node_t *elem = NULL;
+	hash_node_t *new_node = NULL;
 
 	if (ht == NULL || key == NULL || (strcmp(key, "") == 0))
 	{
